32164fa66f0c362c7cf0f13b490a5b27f4ba5779
Jonathan Medina <jonathan.medina@despegar.com>
2019-06-03 20:17:50 -0300

    Almost killed sdl!

---
+++ SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/AnimatedSprite.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/AnimatedSprite.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/BlockView.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/BlockView.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/Camera.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/Camera.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/ChellAnimationView.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/ChellAnimationView.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/Renderer.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/Renderer.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/SDLException.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/SDLException.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/Sprite.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/Sprite.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/SpriteFactory.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/SpriteFactory.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/TextureFactory.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/TextureFactory.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/View.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/View.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/Window.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/Window.cpp.o"
+  "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/WorldView.cpp" "/home/jonathanmedina/Desktop/TPFINAL/TP-Final-Taller-I/SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/WorldView.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SDL/cmake-build-debug/CMakeFiles/sdldemo.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdldemo.dir/Renderer.cpp.o"
+  "CMakeFiles/sdldemo.dir/SDLException.cpp.o"
+  "CMakeFiles/sdldemo.dir/Window.cpp.o"
+  "CMakeFiles/sdldemo.dir/Sprite.cpp.o"
+  "CMakeFiles/sdldemo.dir/AnimatedSprite.cpp.o"
+  "CMakeFiles/sdldemo.dir/ChellAnimationView.cpp.o"
+  "CMakeFiles/sdldemo.dir/TextureFactory.cpp.o"
+  "CMakeFiles/sdldemo.dir/SpriteFactory.cpp.o"
+  "CMakeFiles/sdldemo.dir/WorldView.cpp.o"
+  "CMakeFiles/sdldemo.dir/View.cpp.o"
+  "CMakeFiles/sdldemo.dir/Camera.cpp.o"
+  "CMakeFiles/sdldemo.dir/BlockView.cpp.o"
+  "libsdldemo.pdb"
+  "libsdldemo.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdldemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
