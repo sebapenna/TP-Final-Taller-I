b54f855202fbd8fa572af2ac721150a1ffaf626a
Sebastian Penna <sebapenna19@gmail.com>
2019-06-05 15:40:20 -0300

    ProtectedQueue add

--- Resources/ProtectedQueue.cpp
+++ Resources/ProtectedQueue.cpp
@@ -1,28 +1,14 @@
-#include <iostream>
+#include "ProtectedQueue.h"
 #include <thread>
 #include <string>
-#include "ProtectedQueue.h"
 
-ProtectedQueue::ProtectedQueue() {
-    this->_pushing_finished = false;
-}
-
-void ProtectedQueue::setFinishedAdding() {
-    std::unique_lock<std::mutex> lock(_m);
-    this->_pushing_finished = true;
-    this->_cond_var.notify_all();
-}
-
-void ProtectedQueue::push(void *new_data) {
+void ProtectedBlockingQueue::push(void *new_data) {
     std::lock_guard<std::mutex> lock(_m);
     this->_queue.push(std::move(new_data));
-    this->_cond_var.notify_one();
 }
 
-void * ProtectedQueue::getTopAndPop() {
+void * ProtectedBlockingQueue::getTopAndPop() {
     std::unique_lock<std::mutex> lock(_m);
-    while (this->_queue.empty() && !_pushing_finished)
-        this->_cond_var.wait(lock);
     void* aux = nullptr;
     if (!this->_queue.empty()) {
         aux = _queue.front();
