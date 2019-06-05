631c81e992cc7cf5be114a212a7400b0ac81a574
Sebastian Penna <sebapenna19@gmail.com>
2019-06-05 15:40:00 -0300

    ProtectedQueue and ProtectedBlockingQueue

--- Resources/ProtectedQueue.h
+++ Resources/ProtectedQueue.h
@@ -1,5 +1,5 @@
-#ifndef TP2_PROTECTED_SCRIPTS_PRIORITY_QUEUE_H
-#define TP2_PROTECTED_SCRIPTS_PRIORITY_QUEUE_H
+#ifndef PORTAL_PROTECTEDQUEUE_H
+#define PORTAL_PROTECTEDQUEUE_H
 
 #include <condition_variable>
 #include <queue>
@@ -7,25 +7,18 @@
 // Monitor de la cola, responsable de agregar los elementos e
 // interpretar los mismos controlando los posibles threads a traves de
 // condition variable
-class ProtectedQueue {
+class ProtectedBlockingQueue {
     std::queue<void*> _queue;
-    std::condition_variable _cond_var;
     std::mutex _m;
-    bool _pushing_finished;
 
-    public:
-        ProtectedQueue();
+public:
+    // Agrega un elemeto a la cola
+    void push(void *new_data);
 
-        // Setea que no hay mas elementos por agregar, se asegura que todos los
-        // elementos hayan sido procesados
-        void setFinishedAdding();
-
-        // Agrega un elemeto a la cola
-        void push(void *new_data);
-
-        // Retorna un elemento de la cola y lo quita.
-        // Retorna nullptr en caso que la cola este vacia
-        void * getTopAndPop();
+    // Retorna un elemento de la cola y lo quita.
+    // Retorna nullptr en caso que la cola este vacia
+    void * getTopAndPop();
 };
 
-#endif //TP2_PROTECTED_SCRIPTS_PRIORITY_QUEUE_H
+
+#endif //PORTAL_PROTECTEDQUEUE_H
